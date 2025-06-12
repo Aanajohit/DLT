524ecdc2c941ac6b471de81a89b206b0b3c2011a
Aana Johit <aanajohit0211@gmail.com>
2025-06-12 09:32:58 +0530

    Add files via upload

---
+++ outex3.py
@@ -0,0 +1,41 @@
+Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("Johit")
+Johit
+import numpy as np
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    import numpy as np
+ModuleNotFoundError: No module named 'numpy'
+>>> 
+>>> 
+>>> import numpy as np
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    import numpy as np
+ModuleNotFoundError: No module named 'numpy'
+>>> 
+====== RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python313/ex3.py =====
+Traceback (most recent call last):
+  File "C:/Users/hp/AppData/Local/Programs/Python/Python313/ex3.py", line 1, in <module>
+    import numpy as np
+ModuleNotFoundError: No module named 'numpy'
+>>> 
+====== RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python313/ex3.py =====
+Traceback (most recent call last):
+  File "C:/Users/hp/AppData/Local/Programs/Python/Python313/ex3.py", line 1, in <module>
+    import numpy as np
+ModuleNotFoundError: No module named 'numpy'
+>>> 
+====== RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python313/ex3.py =====
+Epoch 0, Loss: 1.1602
+Epoch 100, Loss: 0.0125
+Epoch 200, Loss: 0.0048
+Epoch 300, Loss: 0.0023
+Epoch 400, Loss: 0.0014
+Epoch 500, Loss: 0.0011
+Epoch 600, Loss: 0.0008
+Epoch 700, Loss: 0.0007
+Epoch 800, Loss: 0.0006
+Epoch 900, Loss: 0.0005
+Test Accuracy: 98.25%
